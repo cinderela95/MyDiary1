ae518145ef47d2c4d9e177ebc650c9e0b61d8eca
Apple <apple@Apples-MacBook-Pro.local>
2020-06-17 14:05:40 +0700

    Initial Commit

---
+++ MyDiary/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyDiary
+//
+//  Created by Apple on 6/17/20.
+//  Copyright © 2020 Minh Thang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
